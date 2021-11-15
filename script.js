f421da2d2ec2c791f227ee0ce6b1bd0d9edea86d
MartinSugasti <martinsugasti@gmail.com>
2021-11-15 15:05:33 +0100

    Add all features with basic design.

---
+++ script.js
@@ -0,0 +1,139 @@
+// DOM elements and global variables
+const booksList = document.querySelector('#books-list')
+const showFormButton = document.querySelector('#show-form-button')
+const addBookForm = document.querySelector('#add-book-form')
+let myLibrary = []
+
+
+// Choose preloaded books
+defaultBooks = [
+  {
+    title: 'Sapiens',
+    author: 'Yuval Noah Harari',
+    pages: 314,
+    read: true
+  },
+  {
+    title: 'The Lord of the Rings',
+    author: 'J. R. R. Tolkien',
+    pages: 309,
+    read: false
+  },
+  {
+    title: 'Harry Potter and the deathly hallows',
+    author: 'J. K. Rowling',
+    pages: 783,
+    read: true
+  },
+  {
+    title: 'One Hundred Years of Solitude',
+    author: 'Gabriel Garcia Marquez',
+    pages: 419,
+    read: true
+  },
+  {
+    title: '1914',
+    author: 'Margaret MacMillan',
+    pages: 892,
+    read: false
+  }
+]
+
+let storedLibrary = JSON.parse(localStorage.getItem('myLibrary'))
+preloadedBooks = storedLibrary === null ? defaultBooks : storedLibrary
+preloadedBooks.forEach((info) => {
+  const book = new Book(info.title, info.author, info.pages, info.read)
+  addBookToLibrary(book)
+})
+
+
+// Functionality
+showFormButton.addEventListener('click', () => {
+  addBookForm.classList.toggle("hidden")
+})
+
+addBookForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const book = new Book(
+    document.querySelector('[name=title]').value,
+    document.querySelector('[name=author]').value,
+    document.querySelector('[name=pages]').value,
+    document.querySelector('[name=read]').checked
+  )
+  addBookToLibrary(book)
+
+  event.target.reset()
+  addBookForm.classList.toggle("hidden")
+})
+
+
+// Functions
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+Book.prototype.toggleRead = function() {
+  this.read = !this.read
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+  displayBooks()
+}
+
+function displayBooks() {
+  booksList.innerHTML = ''
+  myLibrary.forEach((book, index) => {
+    addBookToTable(book, index)
+  })
+}
+
+function addBookToTable(book, rowIndex) {
+  const row = booksList.insertRow(rowIndex)
+  for (const [index, [key, value]] of Object.entries(Object.entries(book))) {
+    let cell = row.insertCell(index)
+
+    if (key === 'read') {
+      addReadButton(cell, value, rowIndex)
+    } else {
+      cell.innerHTML = value
+    }
+  }
+  addDeleteButton(row, rowIndex)
+}
+
+function addReadButton(cell, value, index) {
+  cell.innerHTML = value ? '✓' : '✗'
+  cell.addEventListener('click', () => {
+    toggleReadByIndex(index)
+  })
+}
+
+function toggleReadByIndex(index) {
+  myLibrary[index].toggleRead()
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+  displayBooks()
+}
+
+function addDeleteButton(row, index) {
+  let button = document.createElement('input')
+  button.type = "button"
+  button.value = "Remove"
+  button.addEventListener('click', () => {
+    removeBookFromLibraryByIndex(index)
+  })
+  row.appendChild(button)
+}
+
+function removeBookFromLibraryByIndex(index) {
+  myLibrary.splice(index, 1)
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+  displayBooks()
+}
+
+
